91b56aeb894687a079e4e044f6911cb07c6a883c
Andres Quintero <quand.mind@gmail.com>
2024-08-01 14:59:55 -0400

    changes to events

--- src/db/Events.js
+++ src/db/Events.js
@@ -764,7 +764,7 @@ const getQuotesById = async (id) => {
         cnotificacion: id
       },
       attributes: [
-        'ccotizacion', 'cproveedor', 'xproveedor', 'xobservacion', 'mmontototal', 'itiporeporte', 'xservicio' 
+        'ccotizacion', 'cproveedor', 'xproveedor', 'xobservacion', 'mmontototal', 'itiporeporte', 'xservicio', 'cestatusgeneral', 'xestatusgeneral'
       ],
     });
     const quotes = cotizaciones.map((item) => item.get({ plain: true }));
